5e124098de324f85ae5f4112139280a219288051
Sriram Venkataramani <yuhup2edy@NJTRGCITLAP011.nrtss.com>
2020-05-27 17:22:39 -0400

    Added DB connections

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const db = "mongodb+srv://sriram:pwsriram@cluster0-ver7x.mongodb.net/eventsdb?retryWrites=true&w=majority";
+
+mongoose.connect(db,err=>{
+    //mongoose.connect(db,(err)=>{ 
+    if(err)
+    {
+        console.log("Mongo DB Connect Error "+ err);
+    }
+    else
+    {
+        console.log("Successfully connected to DB");
+    }
+})
 
 router.get('/',(req,res)=>{
     res.send("Response coming from the API routes file on server");
