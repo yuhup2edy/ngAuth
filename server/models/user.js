8c7dc030d0886905ec78e56ab1eb4d85f39aeb0d
Sriram V <xsriram@gmail.com>
2020-05-28 01:03:09 -0400

    Added backend logic for API's

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
     {
-        email : String;
+        email : String,
         password : String
     }
 );
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
 const mongoose = require('mongoose');
+const User = require('../models/user');
 
 const db = "mongodb+srv://sriram:pwsriram@cluster0-ver7x.mongodb.net/eventsdb?retryWrites=true&w=majority";
 
@@ -18,6 +20,134 @@ mongoose.connect(db,err=>{
 
 router.get('/',(req,res)=>{
     res.send("Response coming from the API routes file on server");
+});
+
+router.post('/register',(req,res) =>{
+    let userData = req.body;    // get the input from the request 
+    let user = new User(userData); // cast it to the model (includes the schema)
+
+    user.save((error,registeredUser) => {
+        if (error)
+        {
+            console.log("Error Received on registering new user" + error );
+        }
+        else
+        {
+            res.status(200).send(registeredUser);
+        }
+
+    });
+
+});
+
+router.post('/login',(req,res)=>{
+
+    let userData = req.body;
+
+    // use findOne method to identify if the id (email) exists in the DB. Do not proceed otherwise
+
+    User.findOne({email : userData.email},(error,matchedUser) =>{
+
+        if(error)
+        {
+            console.log("Error received while matching email id to user on login "+ error);
+        }
+        else
+        {
+            if (!matchedUser)
+            {
+                res.status(401).send("Invalid user Id");
+            }
+            else
+            {
+                if (matchedUser.password !== userData.password)
+                {
+                    res.status(401).send("Invalid Password");
+                }
+                else
+                {
+                    res.status(200).send(matchedUser);
+                }
+            }
+        }
+
+    });
+
+});
+
+router.get('/events',(req,res)=>{
+// return hard coded array of events; 
+
+    let events =[
+        {
+            "_id"           : "1",
+            "name"          : "Auto Expo",
+            "description"   : 'lorem ipsum',
+            "date"          : "2012-01-12T18:25:21:511Z"    
+        },
+        {
+            "_id"           : "2",
+            "name"          : "Taxi Expo",
+            "description"   : 'loreta ipsumae',
+            "date"          : "2014-05-19T18:25:21:511Z"    
+        },
+        {
+            "_id"           : "3",
+            "name"          : "Bus Expo",
+            "description"   : 'Mama Biscoth',
+            "date"          : "2014-05-19T18:25:21:511Z"    
+        },
+        {
+            "_id"           : "4",
+            "name"          : "Lorry Expo",
+            "description"   : 'Gentleman Vadivalue',
+            "date"          : "2014-05-19T18:25:21:511Z"    
+        },
+        {
+            "_id"           : "5",
+            "name"          : "Aeroplane Expo",
+            "description"   : 'Super Mumbo Jumbo',
+            "date"          : "2014-05-19T18:25:21:511Z"    
+        }
+    ];
+    res.json(events); // return this array back to the request
+});
+router.get('/special',(req,res)=>{
+// return hard coded array of events; 
+
+    let events =[
+        {
+            "_id"           : "1",
+            "name"          : "Auto Expo",
+            "description"   : 'lorem ipsum',
+            "date"          : "2012-01-12T18:25:21:511Z"    
+        },
+        {
+            "_id"           : "2",
+            "name"          : "Taxi Expo",
+            "description"   : 'loreta ipsumae',
+            "date"          : "2014-05-19T18:25:21:511Z"    
+        },
+        {
+            "_id"           : "3",
+            "name"          : "Bus Expo",
+            "description"   : 'Mama Biscoth',
+            "date"          : "2014-05-19T18:25:21:511Z"    
+        },
+        {
+            "_id"           : "4",
+            "name"          : "Lorry Expo",
+            "description"   : 'Gentleman Vadivalue',
+            "date"          : "2014-05-19T18:25:21:511Z"    
+        },
+        {
+            "_id"           : "5",
+            "name"          : "Aeroplane Expo",
+            "description"   : 'Super Mumbo Jumbo',
+            "date"          : "2014-05-19T18:25:21:511Z"    
+        }
+    ];
+    res.json(events); // return this array back to the request
 });
 
 module.exports = router;
